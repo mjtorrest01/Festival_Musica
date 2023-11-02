7eaec9ca30316537eececf0ea5b7c2b4333c5823
Michael Torres <michaeljtt@outlook.com>
2023-11-02 11:24:14 -0500

    creando funcion de imagenes avif

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     src("src/scss/**/*.scss") // identifica el archivo SASS
@@ -40,6 +41,18 @@ function versionWebp(done){
     done();
 }
 
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe( avif(opciones) )
+        .pipe( dest("build/img") );
+
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css);
 
@@ -49,4 +62,5 @@ function dev(done){
 exports.css = css; // Mandamos  a llamar la funcion CSS
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp; // Mandamos  a llamar la funcion versionWebp
-exports.dev = parallel( imagenes, versionWebp, dev ); // Mandamos  a llamar la funcion versionWebp y dev en paralelo 
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev ); // Mandamos  a llamar la funcion versionWebp y dev en paralelo 
