439823f359fa808e013a9825ffb1cc505e0cb847
Michael Torres <michaeljtt@outlook.com>
2023-11-03 12:59:01 -0500

    creando funcion JS para mostrar galeria en el HTML

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-alert("Hola Mundo");
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp() {
+    crearGaleria();
+}
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for(let i = 1; i <= 12; i++ ) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="imagen/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="imagen/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen Galeriathumb/">
+        `;
+
+        galeria.appendChild(imagen);
+    }
+}
