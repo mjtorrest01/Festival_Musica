ee5f1f84a1a53aa04d93984a5b0b5fab5ee07d10
Michael Torres <michaeljtt@outlook.com>
2023-11-06 13:07:38 -0500

    creando funcion y estilos a seccion galeria

--- build/js/app.js
+++ build/js/app.js
@@ -17,6 +17,42 @@ function crearGaleria(){
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen Galeriathumb/">
         `;
 
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/grande/${id}.avif" type="imagen/avif">
+            <source srcset="build/img/grande/${id}.webp" type="imagen/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen Galeriathumb/">
+        `;
+
+        const overlay = document.createElement('DIV');
+        overlay.appendChild(imagen);
+        overlay.classList.add('overlay');
+        overlay.onclick = function(){
+            const body = document.querySelector('body');
+            body.classList.remove('fijar-body');
+            overlay.remove();
+        }
+
+        const cerrarModal = document.createElement('P');
+        cerrarModal.textContent = 'X';
+        cerrarModal.classList.add('btn-cerrar');
+        cerrarModal.onclick = function(){
+            const body = document.querySelector('body');
+            body.classList.remove('fijar-body');
+            overlay.remove();
+        }
+        overlay.appendChild(cerrarModal);
+
+        const body = document.querySelector('body');
+        body.appendChild(overlay);
+        body.classList.add('fijar-body');
 }
