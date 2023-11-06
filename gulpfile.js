ca14bae137e849df8041d90412bd2ce87942b995
Michael Torres <michaeljtt@outlook.com>
2023-11-06 17:24:07 -0500

    mejorando archivos CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require("gulp"); // src: para identificar
 // CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes Webp
 const cache = require('gulp-cache');
@@ -12,8 +16,11 @@ const avif = require('gulp-avif');
 
 function css(done){
     src("src/scss/**/*.scss") // identifica el archivo SASS
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) // Complila Sass
+        .pipe(postcss([autoprefixer(), cssnano()])) // comprime el archivo css
+        .pipe(sourcemaps.write('.'))
         .pipe(dest("build/css")) // Guarda el archivo compilado en CSS
 
     done(); //Callback que avisa a gulp cuando llegamos al final
